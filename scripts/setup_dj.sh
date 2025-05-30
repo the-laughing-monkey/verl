c48ac0fe7f99ab2457a68c2180b3cc39bb061626
the-laughing-monkey <18448543+the-laughing-monkey@users.noreply.github.com>
2025-05-30 17:54:05 +0200

    Update setup_dj.sh

--- scripts/setup_dj.sh
+++ scripts/setup_dj.sh
@@ -30,7 +30,7 @@ ulimit -n 65536
 #############################
 
 # Define core required python packages
-PACKAGES="pip wheel packaging setuptools huggingface_hub qwen-vl-utils sgl-kernel mathruler"
+PACKAGES="pip wheel packaging setuptools huggingface_hub qwen-vl-utils sgl-kernel mathruler deepspeed vllm"
 
 echo "Upgrading pip"
 pip install --upgrade pip
