901f806e28db064ab8a4dde678dff5058bee0b96
the-laughing-monkey <18448543+the-laughing-monkey@users.noreply.github.com>
2025-05-31 16:58:03 +0200

    two card script

--- examples/tests/train_grpo_qwen2_5_vl_32b_singlenode_simple_two_cards.sh
+++ examples/tests/train_grpo_qwen2_5_vl_32b_singlenode_simple_two_cards.sh
@@ -41,7 +41,7 @@ python3 -m verl.trainer.main_ppo \
     trainer.logger=['console','wandb'] \
     trainer.project_name='verl_grpo_example_geo3k' \
     trainer.experiment_name='qwen2_5_vl_32b_function_rm' \
-    trainer.n_gpus_per_node=1 \
+    trainer.n_gpus_per_node=2 \
     trainer.nnodes=1 \
     trainer.save_freq=20 \
     trainer.test_freq=5 \
