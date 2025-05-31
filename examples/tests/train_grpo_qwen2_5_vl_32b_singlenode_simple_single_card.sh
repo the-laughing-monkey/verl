986c802bf079578032aebe0c69da89aab31c6b39
the-laughing-monkey <18448543+the-laughing-monkey@users.noreply.github.com>
2025-05-31 15:03:28 +0200

    Update train_grpo_qwen2_5_vl_32b_singlenode_simple_single_card.sh

--- examples/tests/train_grpo_qwen2_5_vl_32b_singlenode_simple_single_card.sh
+++ examples/tests/train_grpo_qwen2_5_vl_32b_singlenode_simple_single_card.sh
@@ -7,7 +7,7 @@ python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
     data.train_files=/data/datasets/geo3k/train.parquet \
     data.val_files=/data/datasets/geo3k/test.parquet \
-    data.train_batch_size=128 \
+    data.train_batch_size=32 \
     data.max_prompt_length=1024 \
     data.max_response_length=2048 \
     data.filter_overlong_prompts=True \
@@ -16,8 +16,8 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.model.path=Qwen/Qwen2.5-VL-32B-Instruct \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
-    actor_rollout_ref.actor.ppo_mini_batch_size=32 \
-    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=5 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=16 \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=8 \
     actor_rollout_ref.actor.use_kl_loss=True \
     actor_rollout_ref.actor.kl_loss_coef=0.01 \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
